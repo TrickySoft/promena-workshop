824970c8bf9d4fdb1b829580bc6d2a0f8fb07c72
Arman Khan <58773113+TrickySoft@users.noreply.github.com>
2022-01-06 15:33:47 +0530

    informative strips

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -22,136 +22,85 @@ const Home = () => {
 <div  className='home__banner'> 
 <Banner />
  </div>
-   {/* <div className='container-fluid d-none d-lg-block'>
-        <div className='row align-items-center py-4 px-xl-5'>
-            <div className='col-lg-3'>
-                <a href='' className='text-decoration-none'>
-                    <h1 className='m-0'><span className='text-primary'>E</span>COURSES</h1>
-                </a>
-            </div>
-            <div className='col-lg-3 text-right'>
-                <div className='d-inline-flex align-items-center'>
-                    <i className='fa fa-2x fa-map-marker-alt text-primary mr-3'></i>
-                    <div className='text-left'>
-                        <h6 className='font-weight-semi-bold mb-1'>Our Office</h6>
-                        <small>123 Street, New York, USA</small>
+
+
+    <div className='container-fluid py-5 clients_red mt-5'>
+        <div className='container py-5'>
+            <div id='testimonial_slider' className='carousel slide' data-ride='carousel'>
+                <ul className='carousel-indicators'>
+                    <li data-target='#testimonial_slider' data-slide-to='0' className=''></li>
+                    <li data-target='#testimonial_slider' data-slide-to='1' className='active'></li>
+                    <li data-target='#testimonial_slider' data-slide-to='2' className=''></li>
+                </ul>
+                <div className='carousel-inner'>
+                    <div className='carousel-item'>
+                        <div className='testomonial_section'>
+
+                            <div className='full testimonial_cont'>
+                                <div className='row align-items-center'>
+                                        <div className='col-lg-5'><img src='img/cat-1.jpg' alt='#'/></div>
+                                    
+                                    <div className=' col-lg-7'>
+                    <div className='text-left mb-4'>
+                        <h5 className='text-primary text-uppercase mb-3' style={{letterSpacing:'5px'}}>Why Choose Name Over Name</h5>
+                        <h1>For Your E-Store</h1>
                     </div>
+                    <p>New to e-Commerce? Everybody is setting up shop in the cloud 
+nowadays. However, building an e-store is not an easy feat. we’ll give e-commerce features and very user-friendly professional interface</p>
+                    <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a>
                 </div>
-            </div>
-            <div className='col-lg-3 text-right'>
-                <div className='d-inline-flex align-items-center'>
-                    <i className='fa fa-2x fa-envelope text-primary mr-3'></i>
-                    <div className='text-left'>
-                        <h6 className='font-weight-semi-bold mb-1'>Email Us</h6>
-                        <small>info@example.com</small>
-                    </div>
-                </div>
-            </div>
-            <div className='col-lg-3 text-right'>
-                <div className='d-inline-flex align-items-center'>
-                    <i className='fa fa-2x fa-phone text-primary mr-3'></i>
-                    <div className='text-left'>
-                        <h6 className='font-weight-semi-bold mb-1'>Call Us</h6>
-                        <small>+012 345 6789</small>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> */}
-    {/* <div className='container-fluid'>
-        <div className='row border-top px-xl-5'>
-            <div className='col-lg-3 d-none d-lg-block'>
-                <a className='d-flex align-items-center justify-content-between bg-secondary w-100 text-decoration-none' data-toggle='collapse' href='#navbar-vertical' style={{height: '67px', padding: '0 30px'}}>
-                    <h5 className='text-primary m-0'><i className='fa fa-book-open mr-2'></i>Subjects</h5>
-                    <i className='fa fa-angle-down text-primary'></i>
-                </a>
-                <nav className='collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light' id='navbar-vertical' style={{width: 'calc(100% - 30px)', zIndex: '9'}}>
-                    <div className='navbar-nav w-100'>
-                        <div className='nav-item dropdown'>
-                            <a href='#' className='nav-link' data-toggle='dropdown'>Web Design <i className='fa fa-angle-down float-right mt-1'></i></a>
-                            <div className='dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0'>
-                                <a href='' className='dropdown-item'>HTML</a>
-                                <a href='' className='dropdown-item'>CSS</a>
-                                <a href='' className='dropdown-item'>jQuery</a>
-                            </div>
-                        </div>
-                        <a href='' className='nav-item nav-link'>Apps Design</a>
-                        <a href='' className='nav-item nav-link'>Marketing</a>
-                        <a href='' className='nav-item nav-link'>Research</a>
-                        <a href='' className='nav-item nav-link'>SEO</a>
-                    </div>
-                </nav>
-            </div>
-            <div className='col-lg-9'>
-                <nav className='navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0'>
-                    <a href='' className='text-decoration-none d-block d-lg-none'>
-                        <h1 className='m-0'><span className='text-primary'>E</span>COURSES</h1>
-                    </a>
-                    <button type='button' className='navbar-toggler' data-toggle='collapse' data-target='#navbarCollapse'>
-                        <span className='navbar-toggler-icon'></span>
-                    </button>
-                    <div className='collapse navbar-collapse justify-content-between' id='navbarCollapse'>
-                        <div className='navbar-nav py-0'>
-                            <a href='index.html' className='nav-item nav-link active'>Home</a>
-                            <a href='about.html' className='nav-item nav-link'>About</a>
-                            <a href='course.html' className='nav-item nav-link'>Courses</a>
-                            <a href='teacher.html' className='nav-item nav-link'>Teachers</a>
-                            <div className='nav-item dropdown'>
-                                <a href='#' className='nav-link dropdown-toggle' data-toggle='dropdown'>Blog</a>
-                                <div className='dropdown-menu rounded-0 m-0'>
-                                    <a href='blog.html' className='dropdown-item'>Blog List</a>
-                                    <a href='single.html' className='dropdown-item'>Blog Detail</a>
                                 </div>
                             </div>
-                            <a href='contact.html' className='nav-item nav-link'>Contact</a>
                         </div>
-                        <a className='btn btn-primary py-2 px-4 ml-auto d-none d-lg-block' href=''>Join Now</a>
                     </div>
-                </nav>
+
+                    <div className='carousel-item'>
+                        <div className='testomonial_section'>
+
+                            <div className='full testimonial_cont'>
+                                <div className='row align-items-center'>
+                                        <div className='col-lg-5'><img src='img/cat-2.jpg' alt='#'/></div>
+                                    
+                                    <div className=' col-lg-7'>
+                    <div className='text-left mb-4'>
+                        <h5 className='text-primary text-uppercase mb-3' style={{letterSpacing:'5px'}}>Why Choose Name Over Name</h5>
+                        <h1>For Your E-Store</h1>
+                    </div>
+                    <p>New to e-Commerce? Everybody is setting up shop in the cloud 
+nowadays. However, building an e-store is not an easy feat. we’ll give e-commerce features and very user-friendly professional interface</p>
+                    <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a>
+                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='carousel-item'>
+                        <div className='testomonial_section'>
+
+                            <div className='full testimonial_cont'>
+                                <div className='row align-items-center'>
+                                        <div className='col-lg-5'><img src='img/cat-3.jpg' alt='#'/></div>
+                                    
+                                    <div className=' col-lg-7'>
+                    <div className='text-left mb-4'>
+                        <h5 className='text-primary text-uppercase mb-3' style={{letterSpacing:'5px'}}>Why Choose Name Over Name</h5>
+                        <h1>For Your E-Store</h1>
+                    </div>
+                    <p>New to e-Commerce? Everybody is setting up shop in the cloud 
+nowadays. However, building an e-store is not an easy feat. we’ll give e-commerce features and very user-friendly professional interface</p>
+                    <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a>
+                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
-    </div> */}
-    {/* <div className='container-fluid p-0 pb-5 mb-5'>
-        <div id='header-carousel' className='carousel slide carousel-fade' data-ride='carousel'>
-            <ol className='carousel-indicators'>
-                <li data-target='#header-carousel' data-slide-to='0' className='active'></li>
-                <li data-target='#header-carousel' data-slide-to='1'></li>
-                <li data-target='#header-carousel' data-slide-to='2'></li>
-            </ol>
-            <div className='carousel-inner'>
-                <div className='carousel-item active' style={{minHeight: '300px'}}>
-                    <img className='position-relative w-100' src='img/carousel-1.jpg' style={{minHeight: '300px', objectFit: 'cover'}}/>
-                    <div className='carousel-caption d-flex align-items-center justify-content-center'>
-                        <div className='p-5' style={{width:'100%', maxWidth:'900px'}}>
-                            <h5 className='text-white text-uppercase mb-md-3'>Best Online Courses</h5>
-                            <h1 className='display-3 text-white mb-md-4'>Best Education From Your Home</h1>
-                            <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a>
-                        </div>
-                    </div>
-                </div>
-                <div className='carousel-item' style={{minHeight: '300px'}}>
-                    <img className='position-relative w-100' src='img/carousel-2.jpg' style={{minHeight: '300px', objectFit: 'cover'}}/>
-                    <div className='carousel-caption d-flex align-items-center justify-content-center'>
-                        <div className='p-5' style={{width: '100%', maxWidth: '900px'}}>
-                            <h5 className='text-white text-uppercase mb-md-3'>Best Online Courses</h5>
-                            <h1 className='display-3 text-white mb-md-4'>Best Online Learning Platform</h1>
-                            <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a>
-                        </div>
-                    </div>
-                </div>
-                <div className='carousel-item' style={{minHeight: '300px'}}>
-                    <img className='position-relative w-100' src='img/carousel-3.jpg' style={{minHeight: '300px', objectFit: 'cover'}}/>
-                    <div className='carousel-caption d-flex align-items-center justify-content-center'>
-                        <div className='p-5' style={{width:'100%', maxWidth:'900px'}}>
-                            <h5 className='text-white text-uppercase mb-md-3'>Best Online Courses</h5>
-                            <h1 className='display-3 text-white mb-md-4'>New Way To Learn From Home</h1>
-                            <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> */}
+    </div>
 <About/>
     <div className='container-fluid py-5'>
         <div className='container pt-5 pb-3'>
@@ -162,7 +111,7 @@ const Home = () => {
             <div className='row'>
                 <div className='col-lg-3 col-md-6 mb-4'>
                     <div className='cat-item position-relative overflow-hidden rounded mb-2'>
-                        <img className='img-fluid' src='img/cat-1.jpg' alt=''/>
+                        <img className='img-fluid' src='img/cat-1.jpg'  alt=''/>
                         <a onClick={scrollToSection} className='cat-overlay text-white text-decoration-none'>
                             <h4 className='text-white font-weight-medium'>Web Design</h4>
                             <span>100 + Websites</span>
