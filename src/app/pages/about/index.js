67b8da818df66783bd2a841002916bf5221983e5
santosh <santoshkumar95432@gmail.com>
2022-01-07 16:17:08 +0530

    banner and about

--- src/app/pages/about/index.js
+++ src/app/pages/about/index.js
@@ -9,21 +9,26 @@ import slide5 from '../../../assets/images/beauty1.png';
 const About = () => {
     return (
         <div>
-        <div className='container-fluid  py-3 '>
-        <div className='container py-5'>
-            <div className='row align-items-center'>
-                <div className='col-lg-5'>
-                    <img className='img-fluid rounded mb-4 mb-lg-0' src='img/cat-3.jpg' alt=''/>
-                </div>
-                <div className='col-lg-7'>
-                    <div className='text-left mb-4'>
-                        <h5 className='text-primary text-uppercase mb-3' style={{letterSpacing:'5px'}}>Why Choose Name Over Name</h5>
-                        <h1>For Your E-Store</h1>
+            <div className='container-fluid  py-5 '>
+                <div className='container py-5'>
+                    <div className='row align-items-center'>
+                        <div className='col-lg-5'>
+                            <img className='img-fluid rounded mb-4 mb-lg-0' src='img/cat-3.jpg' alt='' />
+                        </div>
+                        <div className='col-lg-7'>
+                            <div className='text-left mb-4'>
+                                <h5 className='text-primary text-uppercase mb-3' style={{ letterSpacing: '5px' }}>Why Choose Name Over Name</h5>
+                                <h1>For Your E-Store</h1>
+                                <p>Salesforce B2C Commerce can help you create unified, intelligent digital commerce experiences — both online and in the store.</p>
+                                <p>Convert more shoppers by leveraging social extensions, optimising for mobile, empowering a world-class customer service team,
+                                    and add a personal touch with product recommendations.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className='container-fluid py-5 '>
-                <div className='container py-5'>
+            <div className='container-fluid py-2 '>
+                <div className='container py-2'>
                     <div className='row align-items-center'>
                         <div className='col-lg-5'>
                             <div id='header-carousel' className='carousel slide carousel-fade' data-ride='carousel'>
@@ -56,15 +61,15 @@ const About = () => {
                                         </div>
                                     </div>
                                     <div className='carousel-item' style={{ minHeight: '300px' }}>
-                                    <img className='position-relative w-100' src={slide3} alt='image' style={{ minHeight: '300px', objectFit: 'cover' }} />
-                                    <div className='carousel-caption d-flex align-items-center justify-content-center'>
-                                        <div className='p-5' style={{ width: '100%', maxWidth: '900px' }}>
-                                            <h5 className='text-white text-uppercase mb-md-3'>JEWELLERY AND HANDICRAFT ITEMS</h5>
-                                            {/* <h1 className='display-3 text-white mb-md-4'>Best Online Learning Platform</h1> */}
-                                            {/* <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a> */}
+                                        <img className='position-relative w-100' src={slide3} alt='image' style={{ minHeight: '300px', objectFit: 'cover' }} />
+                                        <div className='carousel-caption d-flex align-items-center justify-content-center'>
+                                            <div className='p-5' style={{ width: '100%', maxWidth: '900px' }}>
+                                                <h5 className='text-white text-uppercase mb-md-3'>JEWELLERY AND HANDICRAFT ITEMS</h5>
+                                                {/* <h1 className='display-3 text-white mb-md-4'>Best Online Learning Platform</h1> */}
+                                                {/* <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a> */}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
                                     <div className='carousel-item' style={{ minHeight: '300px' }}>
                                         <img className='position-relative w-100' src={slide4} alt='image' style={{ minHeight: '300px', objectFit: 'cover' }} />
                                         <div className='carousel-caption d-flex align-items-center justify-content-center'>
@@ -101,8 +106,8 @@ const About = () => {
             </div>
 
 
-            <div className='container-fluid  py-5'>
-                <div className='container py-5'>
+            <div className='container-fluid  py-3'>
+                <div className='container py-3'>
                     <div className='row align-items-center'>
 
                         <div className='col-lg-7'>
@@ -127,49 +132,37 @@ const About = () => {
                 </div>
             </div>
             {/* informative strips */}
-            <div className='container-fluid mb-5 py-5'>
+            < div className='container-fluid mb-1 py-1' >
                 <div className='container'>
                     <div className='row align-itmes-center py-3 justify-content-center'>
                         <h1 className='col-lg-8 col-sm-10 col-md-12 font-size text-center'>What will you get here ?</h1>
                     </div>
-                    <p>With 9 years of experience in e-commerce we are confidant to bring the theme with the heighest conversion rate. Set-up a fully functional awsome website without needing to code a single line.</p>
-                    <a href='' className='btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2'>Learn More</a>
+                    <div className='row align-itmes-center justify-content-center card-background'>
+                        <div className='col-lg-3 col-sm-3 col-md-3 text-center box-shadow ml-1'>
+                            <div className='info-card'>
+                                <h1>01</h1>
+                                <i className='fa fa-list fa-icon' aria-hidden='true'></i>
+                            </div>
+                            <span className='card-heading'>Catalouge Managed by Brand Owner</span>
+                        </div>
+                        <div className='col-lg-3 col-sm-3 col-md-3 text-center box-shadow ml-1'>
+                            <div className='info-card'>
+                                <h1>02</h1>
+                                <i className='fa fa-book fa-icon' aria-hidden='true'></i>
+                            </div>
+                            <span className='card-heading'>Money Deposited in 1 Account</span>
+                        </div>
+                        <div className='col-lg-3 col-sm-3 col-md-3 text-center box-shadow ml-1'>
+                            <div className='info-card'>
+                                <h1>03</h1>
+                                <i className='fa fa-user fa-icon' aria-hidden='true'></i>
+                            </div>
+                            <span className='card-heading'>Single Control Panel</span>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    {/* informative strips */}
-    <div className='container-fluid mb-5 py-3'>
-        <div className='container'>
-        <div className='row align-itmes-center py-3 justify-content-center'>
-            <h1 className='col-lg-8 col-sm-10 col-md-12 font-size text-center'>What will you get here ?</h1>
-            </div>
-            <div className='row align-itmes-center justify-content-center card-background'>
-            <div className='col-lg-3 col-sm-3 col-md-3 text-center box-shadow ml-1'>
-      <div className='card'>
-    <h1>01</h1>
-    <i className='fa fa-list fa-icon' aria-hidden='true'></i>
-      </div>
-            <span className='card-heading'>Catalouge Managed by Brand Owner</span>
-            </div>
-            <div className='col-lg-3 col-sm-3 col-md-3 text-center box-shadow ml-1'>
-            <div className='card'>
-    <h1>02</h1>
-    <i className='fa fa-book fa-icon' aria-hidden='true'></i>
-      </div>
-            <span className='card-heading'>Money Deposited in 1 Account</span>
-            </div>
-            <div className='col-lg-3 col-sm-3 col-md-3 text-center box-shadow ml-1'>
-            <div className='card'>
-    <h1>03</h1>
-    <i className='fa fa-user fa-icon' aria-hidden='true'></i>
-      </div>
-            <span className='card-heading'>Single Control Panel</span>
-            </div>
-            </div>
-        </div>
-    </div>
-    </div> 
+            </div >
+        </div >
 
     )
 }
